934c613c631b7698b75622e459acf1b859b7dd48
eureka <binwenliu.ai@gmail.com>
2025-08-22 19:49:01 +0800

    add

--- scripts/train_eeg_pair.sh
+++ scripts/train_eeg_pair.sh
@@ -16,7 +16,7 @@ python "$PY" \
   checkpoint.checkpoint_path=/root/autodl-tmp/checkpoint/encodec_maestro_4gpus_ep150_256_4/bs8_cut72000_length0_epoch150_lr0.0003.pt \
   optimization.lr=1e-4 optimization.disc_lr=1e-4 \
   common.max_epoch=20 \
-  checkpoint.save_folder=/root/code/encodec-pytorch/checkpoints \
-  tensorboard.log_dir=/root/code/encodec-pytorch/runs
+  checkpoint.save_folder=/root/autodl-tmp/checkpoints \
+  tensorboard.log_dir=/root/autodl-tmp/run
 
 
